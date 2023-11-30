031a2ffcea761dca4bc7b25b125ea1e20aad2c31
Nelson Correa <nelsonc897@gmail.com>
2023-11-30 18:14:42 -0500

    reload-login--

--- src/app/public/login-registro/containers/login-registro.component.ts
+++ src/app/public/login-registro/containers/login-registro.component.ts
@@ -88,7 +88,10 @@ export class LoginRegistroComponent {
           } else if (response.rol === 2) {
             this.router.navigate(['doctor-listar-citas']);
           } else {
-            this.router.navigate(['home']);
+            this.router.navigate(['home']).then(() => {
+              // Recargar la página después de la navegación
+              window.location.reload();
+            });
           }
 
           console.log(response);
