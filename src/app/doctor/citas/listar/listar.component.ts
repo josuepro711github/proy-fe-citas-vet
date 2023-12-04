75d8cff61b01f630e6e40a43eb97ab19cf2ce38a
Nelson Correa <nelsonc897@gmail.com>
2023-12-03 20:34:51 -0500

    ayuda!xd ingresar observaciones--

--- src/app/doctor/citas/listar/listar.component.ts
+++ src/app/doctor/citas/listar/listar.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatCellDef, MatTableDataSource } from '@angular/material/table';
 import { lastValueFrom } from 'rxjs';
+import { InfocitaDoctorComponent } from '../infocita-doctor/infocita-doctor.component';
 
 
 @Component({
@@ -21,15 +23,29 @@ export class ListarComponent {
     this.dataSource.paginator = this.paginator;
   }
 
+  constructor( public dialog: MatDialog) {
+    
+  }
+
 
   cancelarCita() {
     console.log('Cancelar cita: ');
     // Aquí puedes agregar el código para cancelar la cita
   }
   
-  abrirDetalle() {
-    console.log('Mostrar más detalles: ');
+  abrirDetalle(element: any) {
+    
     // Aquí puedes agregar el código para mostrar más detalles
+  }
+
+  diagnosticarCita( element: any) {
+    // agregar el modal
+    this.dialog.open(InfocitaDoctorComponent, {
+      data: element,
+      minWidth: '60%',
+      // panelClass: transparente
+      panelClass: 'custom-dialog-container',
+    })
   }
 
 
@@ -42,11 +58,12 @@ export interface Element {
   fecha: string;
   hora: string;
   estado: string;
+  observaciones: string;
 }
 
 
 const ELEMENT_DATA: Element[] = [
-  {duenio: 'Juan', mascota: 'Firulais', motivo: 'Chequeo', fecha: '2023-12-03', hora: '15:00', estado: 'Confirmado'},
-{duenio: 'Maria', mascota: 'Manchas', motivo: 'Vacunación', fecha: '2023-12-04', hora: '10:00', estado: 'Pendiente'},
+  {duenio: 'Juan', mascota: 'Firulais', motivo: 'Chequeo', fecha: '2023-12-03', hora: '15:00', estado: 'Confirmado', observaciones: 'La mascota se encuentra en buenas condiciones'},
+{duenio: 'Maria', mascota: 'Manchas', motivo: 'Vacunación', fecha: '2023-12-04', hora: '10:00', estado: 'Pendiente' , observaciones: 'La mascota se encuentra en buenas condiciones'},
   
 ];
